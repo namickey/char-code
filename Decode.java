ae1ad8e3fff942c7f015bbff3d3f0e11d4d95b3a
namickey <teethdai@gmail.com>
2019-06-11 23:37:11 +0900

    add decode.

---
+++ Decode.java
@@ -0,0 +1,9 @@
+import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.Charset;
+
+public class Decode {
+    public static void main(String[] args) {
+        String hexStr = "8D848B429670E6638141";
+        System.out.println(new String(DatatypeConverter.parseHexBinary(hexStr), Charset.forName("Shift-JIS")));
+    }
+}
